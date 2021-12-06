3d00af7868d2c1ef6e423f7af12bf6616bcb6722
Blake Mitchell <blake.mitchell@wildflowerhealth.com>
2021-12-06 11:22:01 -0800

    decorator defaults

--- src/decorator.ts
+++ src/decorator.ts
@@ -3,7 +3,7 @@ import { Extensions } from 'type-graphql';
 import { IApongoLookup } from './models';
 
 /** add apongo metadata to an entity */
-export function Apongo(lookup: IApongoLookup, compose: string[], expr: string): Function {
+export function Apongo(lookup: IApongoLookup, compose: string[] = [], expr?: string): Function {
     // factory returns the actual decoration function.
     return function (target: Function): void {
         Extensions({
